d20574baabe18f5e21fdc27f5119f35dded40099
username <2810277205@qq.com>
2022-09-02 14:12:43 +0800

    one

---
+++ src/main/java/com/example/gitdemo2/GitDemo2Application.java
@@ -0,0 +1,14 @@
+package com.example.gitdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo2Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(GitDemo2Application.class, args);
+    }
+
+}
